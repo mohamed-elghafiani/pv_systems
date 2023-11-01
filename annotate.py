d6a6adf6ba83045b555f52c39d23a6892cffa534
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2023-11-01 11:24:58 +0100

    one-hot encoding

---
+++ annotate.py
@@ -0,0 +1,26 @@
+import scipy.io as sio
+
+
+data = sio.loadmat('I_DATA.mat')
+
+an = {
+    'WO': [1.0, 0.0, 0.0, 0.0], 
+    'OM': [0.0, 1.0, 0.0, 0.0], 
+    'RS': [0.0, 0.0, 1.0, 0.0], 
+    'RP': [0.0, 0.0, 0.0, 1.0]
+}
+
+# for key in data.keys():
+#     d = data[key]
+#     new_d = []
+#     if not key.startswith('__'):
+#         for el in d:
+#             new_d.append([el, an[key[:2]]])
+
+#     data[key] = new_d
+
+# sio.savemat('an_I_data.mat', data)
+
+new_data = sio.loadmat('an_I_data.mat')
+
+print(new_data['RS_1'][0])
--- read_data.py
+++ read_data.py
@@ -5,15 +5,15 @@ import re
 # import matplotlib.pyplot as plt
 
 
-DATA_DIRS = ["data1/ombrage", "data1/Rp", "data1/Rs"]
+DATA_DIRS = ["data/ombrage", "data/Rp", "data/Rs"]
 OMB_DATA = []
 
-data_type_key = "resultsI"
+data_type_key = "resultsV"
 data = {}
 for dir_ in DATA_DIRS:
     for i in range(1, 6):
         # load data from data.mat file
-        OMB_DATA_1 = sio.loadmat(f"{DATA_DIRS[0]}/{i}/data{i}.mat")
+        OMB_DATA_1 = sio.loadmat(f"{dir_}/{i}/data{i}.mat")
         # create a key for the data such as: 
         # "OM" --> "ombrage"
         # "RS" --> "Rs" ... etc.
@@ -21,17 +21,17 @@ for dir_ in DATA_DIRS:
         # get the right key for data we need to read
         # Problem is not always we have the same key: 
         # resultsI2? results4? results3? ...
-        data_type_key = get_correct_key(OMB_DATA_1.keys(), data_type_key)
+        data_key = get_correct_key(OMB_DATA_1.keys(), data_type_key)
         # restructure the data in the form of a dictionary such as:
         # {
         #    "OM": [LIST OF OMBRAGE-TYPE DEFECT SIMULATION DATA],
         #    "RS": [LIST OF RS-TYPE DEFECT SIMULATION DATA]
         # }
-        process_to_dict(OMB_DATA_1[data_type_key][0], key, data)
+        process_to_dict(OMB_DATA_1[data_key][0], key, data)
 
 
-OMB_DATA_1 = sio.loadmat("data1/sans_defaut/1/data.mat")
-process_to_dict(OMB_DATA_1["resultsI"][0], "WO", data)
+OMB_DATA_1 = sio.loadmat("data/sans_defaut/1/data.mat")
+process_to_dict(OMB_DATA_1[data_type_key][0], "WO", data)
 
 
 max_num = get_max_num_points(data)
@@ -42,7 +42,8 @@ print(min_num)
 print(data.keys())
 
 for key in data.keys():
-    max_ = len(max(data[key], key=lambda el: len(el)))
+    max_ = 450
+    # max_ = len(max(data[key], key=lambda el: len(el)))
     data_homog = []
     for el in data[key]:
         el = list(el)
@@ -57,9 +58,9 @@ for el in data_homog:
         print("differnt length detected!")
         print(f"===> {len(el)}")
 
-# sio.savemat("I_DATA.mat", data)
+sio.savemat("V_DATA.mat", data)
 
-new_data = sio.loadmat("I_DATA.mat")
+new_data = sio.loadmat("V_DATA.mat")
 print(new_data.keys())
 
 lengths = []
@@ -68,3 +69,5 @@ for key in [k for k in new_data.keys() if not k.startswith("__")]:
 
 print(new_data["OM_1"][0])
 print(lengths)
+
+
